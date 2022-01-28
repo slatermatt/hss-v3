69fa037871020e99ca45ec0b4d61830f55f3a7a0
slatermatt <matthewslater1998@gmail.com>
2022-01-28 00:09:04 +0000

    Adds initial basic content builder

---
+++ components/content/ContentBuilder.vue
@@ -0,0 +1,30 @@
+<template>
+	<article class="prose border-4 border-[#f00]">
+		<component
+			:is="__component.split('.')[1]"
+			v-for="({ __component, ...value }, index) in $props.components"
+			:key="index"
+			class="border-4 border-[#00f] overflow-scroll"
+			v-bind="value"
+		/>
+	</article>
+</template>
+
+<script>
+	import Heading from './Heading';
+	import Paragraph from './Paragraph';
+
+	export default {
+		components: {
+			Heading,
+			Paragraph,
+		},
+
+		props: {
+			components: {
+				type: Array,
+				required: true,
+			},
+		},
+	};
+</script>
