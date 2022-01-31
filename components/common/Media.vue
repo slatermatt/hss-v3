22a0ce1eec4897fa3634c5bb41171f9037966177
slatermatt <matthewslater1998@gmail.com>
2022-01-31 18:29:14 +0000

    Adds media component

---
+++ components/common/Media.vue
@@ -0,0 +1,58 @@
+<template>
+	<placeholder :ratio-class="$props.ratioClass">
+		<component
+			:is="cIsVideo ? 'e-video' : 'e-img'"
+			:url="cUrl"
+			:alternative-text="$props.alternativeText"
+		/>
+	</placeholder>
+</template>
+
+<script>
+	import Video from './Video';
+	import Img from './Img';
+
+	export default {
+		inheritAttrs: false,
+
+		components: {
+			EVideo: Video,
+			EImg: Img,
+		},
+
+		props: {
+			alternativeText: {
+				type: String,
+				required: true,
+			},
+
+			mime: {
+				type: String,
+				required: true,
+			},
+
+			url: {
+				type: String,
+				required: true,
+			},
+
+			ratioClass: {
+				type: String,
+				default: 'aspect-w-16 aspect-h-9',
+			},
+		},
+
+		computed: {
+			cIsVideo() {
+				return this.$props.mime === 'video/mp4';
+			},
+
+			cUrl() {
+				const domain = this.$strapi.options.url;
+				const output = new URL(this.$props.url, domain);
+
+				return output;
+			},
+		},
+	};
+</script>
---
+++ components/common/Video.vue
@@ -0,0 +1,20 @@
+<template>
+	<video
+		class="absolute inset-0 w-full h-full object-cover"
+		:src="$props.url"
+		controls
+	/>
+</template>
+
+<script>
+	export default {
+		inheritAttrs: false,
+
+		props: {
+			url: {
+				type: [String, URL],
+				required: true,
+			},
+		},
+	};
+</script>
