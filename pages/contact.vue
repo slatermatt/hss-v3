a6b6a1750449a62838ab337bc57adb8f59f99fc8
slatermatt <matthewslater1998@gmail.com>
2022-01-31 16:50:31 +0000

    Adds contact page

---
+++ pages/contact.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<pre
+			v-text="$data"
+		/>
+	</div>
+</template>
+
+<script>
+	const qs = require('qs');
+	const query = qs.stringify({ populate: '*' });
+
+	export default {
+		async asyncData({ $strapi }) {
+			const page = await $strapi.find(`api/contact?${query}`);
+
+			return {
+				...page.data.attributes,
+			};
+		},
+	};
+</script>
