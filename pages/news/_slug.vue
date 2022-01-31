7d73b7a113bb1798430ade4e8489bb65264c2d54
slatermatt <matthewslater1998@gmail.com>
2022-01-31 00:52:46 +0000

    Adds news articles model

---
+++ pages/news/_slug.vue
@@ -0,0 +1,35 @@
+<template>
+	<div>
+		<pre
+			v-if="$data.article"
+			v-text="$data"
+		/>
+
+		<p v-else>
+			No article found
+		</p>
+	</div>
+</template>
+
+<script>
+	const qs = require('qs');
+
+	export default {
+		async asyncData({ $strapi, params }) {
+			const query = qs.stringify({
+				populate: '*',
+				filters: {
+					slug: {
+						$eq: params.slug,
+					},
+				},
+			});
+
+			const { data } = await $strapi.find(`api/articles?${query}`);
+
+			return {
+				article: data[0],
+			};
+		},
+	};
+</script>
---
+++ pages/news/index.vue
@@ -0,0 +1,73 @@
+<template>
+	<div>
+		<intro
+			v-if="$data.Intro"
+			v-bind="$data.Intro"
+		/>
+
+		<pre
+			v-if="$data.Articles && $data.Articles.length"
+			v-text="$data.Articles"
+		/>
+
+		<block-builder
+			v-if="$data.Blocks && $data.Blocks.length"
+			:components="$data.Blocks"
+		/>
+
+		<contact-promo
+			v-if="$data.ContactPromo"
+			v-bind="$data.ContactPromo"
+		/>
+	</div>
+</template>
+
+<script>
+	const qs = require('qs');
+
+	import Intro from '@/components/blocks/Intro';
+	import BlockBuilder from '@/components/blocks/BlockBuilder';
+	import ContactPromo from '@/components/blocks/ContactPromo';
+
+	export default {
+		components: {
+			Intro,
+			BlockBuilder,
+			ContactPromo,
+		},
+
+		async asyncData({ $strapi }) {
+			const query = qs.stringify({
+				populate: {
+					Intro: {
+						populate: '*',
+					},
+					Blocks: {
+						populate: '*',
+					},
+					ContactPromo: {
+						populate: '*',
+					},
+				},
+			}, {
+				encodeValuesOnly: true,
+			});
+
+			const content = qs.stringify({
+				populate: {
+					Content: {
+						populate: '*',
+					},
+				},
+			});
+
+			const { data } = await $strapi.find(`api/news-page?${query}`);
+			const articles = await $strapi.find(`api/articles?${content}`);
+
+			return {
+				...data.attributes,
+				Articles: articles.data,
+			};
+		},
+	};
+</script>
