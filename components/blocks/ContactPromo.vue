03a698d4cb5ddb6afb006100073da9462f9f8b52
slatermatt <matthewslater1998@gmail.com>
2022-02-06 18:49:42 +0000

    Adds Contact Promo | Completes Size Estimator page

--- components/blocks/ContactPromo.vue
+++ components/blocks/ContactPromo.vue
@@ -1,7 +1,44 @@
 <template>
-	<pre v-text="$attrs" />
+	<section class="bg-off-white">
+		<div class="container max-w-xl mx-auto text-center py-20">
+			<h2
+				class="text-brand-green text-4xl inline-block font-bold leading-snug"
+				v-text="$props.Title"
+			/>
+
+			<p
+				v-if="$props.Content"
+				class="mt-4"
+				v-html="$props.Content"
+			/>
+
+			<e-button
+				v-if="$props.Cta"
+				:title="$props.Cta.Title"
+				:url="$props.Cta.Url"
+				class="inline-block mt-8"
+			/>
+		</div>
+	</section>
 </template>
 
 <script>
-	export default {};
+	export default {
+		props: {
+			Title: {
+				type: String,
+				required: true,
+			},
+
+			Content: {
+				type: String,
+				required: true,
+			},
+
+			Cta: {
+				type: Object,
+				default: null,
+			},
+		},
+	};
 </script>
