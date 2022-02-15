720fc3f906534eab16fa6b27df89893560dfd22a
slatermatt <matthewslater1998@gmail.com>
2022-02-15 13:58:01 +0000

    Adds error page

---
+++ layouts/error.vue
@@ -0,0 +1,21 @@
+<template>
+	<intro
+		Title="Oops, something went wrong"
+		Content="It looks like this page doesn't exist"
+		:Cta="{
+			Title: 'Back home',
+			Url: '/',
+		}"
+		class="my-12 md:my-18 xl:my-24"
+	/>
+</template>
+
+<script>
+	import Intro from '@/components/blocks/Intro';
+
+	export default {
+		components: {
+			Intro
+		},
+	};
+</script>
